fea5d9357bea288957e705e727ccb3a4230f4367
Maxidonx <johnmaxwell927@gmail.com>
2025-03-24 12:17:44 -0500

    added apis

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from flask_wtf.csrf import CSRFProtect
+# from flask_wtf.csrf import CSRFProtect
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
@@ -16,6 +17,5 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 migrate = Migrate(app, db)
 
-csrf = CSRFProtect(app)
+# csrf = CSRFProtect(app)
 
-# from app import models
